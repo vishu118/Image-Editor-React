2243420c16b976f44e9725b7b4c3712d57b73cba
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-04-17 21:58:14 +0530

    .

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,15 +1,38 @@
-import React from 'react'
-import "./Main.css"
-
+import React from "react";
+import "./Main.css";
 
 const Main = () => {
+  const Filters = [
+    {
+      name: "Brightness",
+    },
+    {
+      name: "Greyscale",
+    },
+    {
+      name: "Sepia",
+    },
+    {
+      name: "Saturate",
+    },
+    {
+      name: "Contrast",
+    },
+    {
+      name: "HueRotate",
+    },
+  ];
   return (
-    <div className='mainContainer'>
+    <div className="mainContainer">
       <div className="wrapper">
-       
+        <h2>Image Editor</h2>
+        <div className="editor-body">
+          <div className="feature-container"></div>
+          <div className="image-container"></div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
