9d609c9d45e97fea49a086eb1b4bc81c4a366f7b
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-04-18 11:04:47 +0530

    basic css

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -22,14 +22,28 @@ const Main = () => {
       name: "HueRotate",
     },
   ];
+
+  let image = "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg"
   return (
     <div className="mainContainer">
       <div className="wrapper">
         <h2>Image Editor</h2>
         <div className="editor-body">
-          <div className="feature-container"></div>
-          <div className="image-container"></div>
+          <div className="feature-container">
+            <span>Filter</span>
+            <div className="fiter_key">
+              {
+                Filters.map((ele,ind)=>{
+                return <button>{ele.name}</button>
+                })
+              }
+            </div>
+          </div>
+          <div className="image-container">
+            <img src={image}  alt="images" style={{width : "60%"}} />
+          </div>
         </div>
+        <div className="footer"></div>
       </div>
     </div>
   );
