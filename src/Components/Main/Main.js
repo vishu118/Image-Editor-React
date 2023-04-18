68accd974bc6c054a99e71a1562be3f58b37e997
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-04-18 11:44:35 +0530

    css

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -23,27 +23,36 @@ const Main = () => {
     },
   ];
 
-  let image = "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg"
+ 
   return (
-    <div className="mainContainer">
-      <div className="wrapper">
-        <h2>Image Editor</h2>
-        <div className="editor-body">
-          <div className="feature-container">
-            <span>Filter</span>
-            <div className="fiter_key">
-              {
-                Filters.map((ele,ind)=>{
-                return <button>{ele.name}</button>
-                })
-              }
+    <div className="image-editor">
+      <div className="card">
+          <div className="card_header">
+            <h2>Image Editor</h2>
+          </div>
+          <div className="card-body">
+            <div className="sidebar">
+              <div className="side_body">
+                <div className="filer_section">
+                  <span>Filters</span>
+                  <div className="filter_features">
+                    {
+                      Filters.map((ele,idx)=>{
+                        return <button key={idx} className="features">{ele.name}</button>
+                      })
+                    }
+                  </div>
+                </div>
+                <div className="filter_slider">
+                                <div className="label_bar">
+                                    <label htmlFor="range">Rotate</label>
+                                    <span>100%</span>
+                                </div>
+                                <input type="range" />
+                            </div>
+              </div>
             </div>
           </div>
-          <div className="image-container">
-            <img src={image}  alt="images" style={{width : "60%"}} />
-          </div>
-        </div>
-        <div className="footer"></div>
       </div>
     </div>
   );
