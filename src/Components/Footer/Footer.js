61375180dba7c4789487b506afd7f3fa926544e0
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-04-19 12:21:20 +0530

    ui part completed

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,14 +6,14 @@ const Footer = () => {
   return (
     <div className='images_option'>
         <div className="reset">
-            <button className='size'>Reset</button>
-            <button className='size'>Save Images</button>
+            <button className='resetbtn'>Reset</button>
+            <button className='Savebtn'>Save Images</button>
         </div>
         <div className="choose">
             <button className='size undo'><IoMdUndo/></button>
             <button className='size redo'><IoMdRedo/></button>
             <button className='size crop'>Crop Image</button>
-            <label htmlFor='choose'>Choose Image</label> 
+            <label htmlFor='choose' className='size'>Choose Image</label> 
             <input type='file' id='choose'/>
         </div>
     </div>
