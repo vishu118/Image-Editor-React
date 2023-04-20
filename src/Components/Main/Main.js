39547b9103d3c06ad0e6c1c8ac2971d05a944743
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-04-20 23:53:59 +0530

    filters

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useRef } from "react";
 import { GrRotateLeft,GrRotateRight } from 'react-icons/gr';
 import { CgMergeVertical,CgMergeHorizontal } from 'react-icons/cg';
 import { IoIosImage } from "react-icons/io";
@@ -7,28 +7,26 @@ import "../Style/Main.css";
 
 
 const Main = () => {
-  const Filters = [
-    {
-      name: "Brightness",
-    },
-    {
-      name: "Greyscale",
-    },
-    {
-      name: "Sepia",
-    },
-    {
-      name: "Saturate",
-    },
-    {
-      name: "Contrast",
-    },
-    {
-      name: "HueRotate",
-    },
+ 
+
+  const filterOptions = [
+    { id: "brightness", name: "Brightness" },
+    { id: "saturation", name: "Saturation" },
+    { id: "inversion", name: "Inversion" },
+    { id: "grayscale", name: "Grayscale" },
+    { id: "contrast", name: "Contrast" },
+    { id: "hueRotate", name: "HueRotate" },
   ];
 
   const [PreviewImg ,setPreviewImg ] = useState(null)
+  const [activeFilter, setActiveFilter] = useState("Brightness");
+  const [sliderValue, setSliderValue] = useState(100);
+  const [brightness, setBrightness] = useState("100");
+  const [saturation, setSaturation] = useState("100");
+  const [inversion, setInversion] = useState("0");
+  const [grayscale, setGrayscale] = useState("0");
+  const PreviewImgRef = useRef(null);
+
 
   
   const loadImage = (e)=>{
@@ -36,8 +34,49 @@ const Main = () => {
     if(!file) return;
     setPreviewImg(file);
   }
+
+  const applyFilter = () => {
+    PreviewImgRef.current.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
+    // previewImgRef.current.style.transform = `rotate(${rotate}deg) scale(${flipHorizontal}, ${flipVertical})`;
+  };
+
+  const handleFilterClick = (option) => {
+    setActiveFilter(option.name);
+
+    switch (option.id) {
+      case "brightness":
+        setSliderValue(brightness);
+        break;
+      case "saturation":
+        setSliderValue(saturation);
+        break;
+      case "inversion":
+        setSliderValue(inversion);
+        break;
+      default:
+        setSliderValue(grayscale);
+    }
+  };
+ 
+  const handleSliderChange = (event) => {
+    setSliderValue(event.target.value);
+    switch (activeFilter) {
+      case "brightness":
+        setBrightness(event.target.value);
+        break;
+      case "saturation":
+        setSaturation(event.target.value);
+        break;
+      case "inversion":
+        setInversion(event.target.value);
+        break;
+      default:
+        setGrayscale(event.target.value);
+    }
+  };
+ 
   return (
-    <div className="image-editor">
+    <div className="image-editor"  >
       <div className="card">
         <div className="card_header">
           <h2>Image Editor</h2>
@@ -48,22 +87,40 @@ const Main = () => {
             <div className="side_body">
               <div className="filer_section">
                 <span>Filters</span>
-                <div className="filter_features">
-                  {Filters.map((ele, idx) => {
+                  <div className="filter_features">
+                  {filterOptions.map((ele, idx) => {
                     return (
-                      <button key={idx} className="features">
-                        {ele.name}
-                      </button>
+                      <button
+                  key={ele.id}
+                  id={ele.id}
+                  className={activeFilter === ele.id ? "active" : ""}
+                  onClick={() => handleFilterClick(ele)}
+                >
+                  {ele.name}
+                </button>
                     );
                   })}
                 </div>
               </div>
+
+
               <div className="filter_slider">
                 <div className="sliders">
-                  <span>Rotate</span>
-                  <span>100%</span>
+                  <p className="name">{activeFilter}</p>
+                  <p className="value">{`${sliderValue}%`}</p>
+                 
                 </div>
-                <input type="range" />
+                <input
+                type="range"
+                min="0"
+                max={
+                  activeFilter === "Brightness" || activeFilter === "saturation"
+                    ? "200"
+                    : "100"
+                }
+                value={sliderValue}
+                onChange={handleSliderChange}
+              />
               </div>
               <div className="rotate">
                    <div className="extra_features">
@@ -86,6 +143,7 @@ const Main = () => {
               <img 
                src={URL.createObjectURL(PreviewImg)}
                alt="preview"
+               
               />
             ):(
               <label htmlFor="choose">
